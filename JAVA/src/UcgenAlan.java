c02401e496c0ec2aaa93c3d9d3654eaa68d65e87
enesdemrr <enesdemr@outlook.com.tr>
2021-11-05 15:33:58 +0300

    3.odev

--- JAVA/src/UcgenAlan.java
+++ JAVA/src/UcgenAlan.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
-public class HipotenusBul {
+public class UcgenAlan {
     public static void main(String[] args) {
-        int a,b;
-        double c;
+        int a,b,c;
         Scanner inp = new Scanner(System.in);
         System.out.print("1. Kenari girin: ");
         a = inp.nextInt();
         System.out.print("2. Kenari girin: ");
         b = inp.nextInt();
-        c = Math.sqrt((a*a) + (b*b));
-        System.out.println("Hipotenus = "+c);
+        System.out.print("3. Kenari girin: ");
+        c = inp.nextInt();
+        float u = (a+b+c)/2;
+        float alan = (float) Math.sqrt(u * (u-a) * (u - b) * (u - c));
+        System.out.println("Alan = "+ alan);
     }
 }
