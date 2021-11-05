a491e76a3fe053f97ae07248a710b60a86375d2b
enesdemrr <enesdemr@outlook.com.tr>
2021-11-05 14:54:41 +0300

    2.odev

--- JAVA/src/KdvHesap.java
+++ JAVA/src/KdvHesap.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class KdvHesap {
     public static void main(String[] args) {
         float fiyat,kdvFiyat;
+        float kdv= 0.18f;
         Scanner inp = new Scanner(System.in);
-        System.out.print("Ürünün Fiyatını girinl: ");
+        System.out.print("Ürünün Fiyatını girin: ");
         fiyat = inp.nextFloat();
-        kdvFiyat = fiyat + (fiyat * 18)/100;
+        kdvFiyat = fiyat + (fiyat * kdv);
+        System.out.println("Kdv'siz fiyat: "+ fiyat);
+        System.out.println("Kdv orani: %"+ kdv*100);
         System.out.println("Kdv'li fiyat: "+ kdvFiyat);
     }
 }
